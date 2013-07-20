6a4ccc147b5238579f82cee920789e9f659d75cf
zafar <fastalam420@gmail.com>
2013-07-20 16:23:32 +1000

    Initial repository for fist app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '9bd7307b76b9d2d5b30a7d4b4c9e9bdfa836acc3f930c3d001b208373dcbb9b25d4aa6c7817a137844d6956685b6a0422756e721db09a5fe5c4cf091737b685b'
