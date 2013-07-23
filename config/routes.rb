0c7f700e053edd4aacaacf9a4cd13ee4a9d0bab9
zafar_alfred <fastalam420@gmail.com>
2013-07-23 16:32:24 +1000

    added static controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 FirstApp::Application.routes.draw do
+  root 'static_pages#home'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+  #get "static_pages/home"
+  #get "static_pages/about"
+  #get "static_pages/contact"
+  #get "static_pages/help"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
